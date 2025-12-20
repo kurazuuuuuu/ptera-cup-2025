d43a333c48f64bfb2e092414bae82554f3acb33c
kurazuuuuuu <110393398+kurazuuuuuu@users.noreply.github.com>
2025-12-21 00:44:51 +0900

    fix: スキーマ修正

--- app/backend/src/schemas.py
+++ app/backend/src/schemas.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 import uuid
 from typing import Optional
 from fastapi_users import schemas
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
     username: str
+    created_at: datetime
 
 class UserCreate(schemas.BaseUserCreate):
     username: str
